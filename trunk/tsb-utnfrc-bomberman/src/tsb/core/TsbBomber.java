40c9758a25c91aa45cf0f0616bacea43e8e7443e
Cebollo3@gmail.com <Cebollo3@gmail.com@8419b626-213f-0b82-6ddc-b2c116e3f213>
2013-07-10 23:24:40 +0000

    Fix de mocos

---
+++ trunk/tsb-utnfrc-bomberman/src/tsb/core/TsbBomber.java
@@ -0,0 +1,32 @@
+package tsb.core;
+
+import com.badlogic.gdx.ApplicationListener;
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author facundo
+ */
+
+public class TsbBomber implements ApplicationListener {
+	        public void create () {
+	        }
+
+	        public void render () {
+	        }
+
+	        public void resize (int width, int height) {
+	        }
+
+	        public void pause () {
+	        }
+
+	        public void resume () {
+	        }
+
+	        public void dispose () {
+	        }
+	}
